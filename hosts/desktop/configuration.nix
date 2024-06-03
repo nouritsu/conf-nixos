99faf9beaff05fea14b4ea461104f0c4e954f637
= <aneesh1701@gmail.com>
2024-06-03 14:00:55 +0530

    restructure order

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -6,9 +6,6 @@
     ../../modules/nvidia.nix
   ];
 
-  # Networking
-  networking.hostName = "nouritsu";
-
   # Experimental Features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -18,15 +15,11 @@
     description = "Aneesh Bhave";
     extraGroups = [ "networkmanager" "wheel" ];
   };
-
-  # Packages
+  networking.hostName = "nouritsu";
   nixpkgs.config.allowUnfree = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  # ================================================================ #
+  # =                         DO NOT TOUCH                         = #
+  # ================================================================ #
+  system.stateVersion = "23.05";
 }
