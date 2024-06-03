a45cf82f5409eb826b7cdb8ec74b6c8156919721
= <aneesh1701@gmail.com>
2024-06-03 13:37:52 +0530

    init commit

---
+++ modules/common.nix
@@ -0,0 +1,20 @@
+{ ... }: {
+  imports = [
+    ./applications.nix
+    ./bootloader.nix
+    ./de.nix
+    ./development.nix
+    ./dm.nix
+    ./fonts.nix
+    ./locale.nix
+    ./neovim.nix
+    ./networking.nix
+    ./pipewire.nix
+    ./services.nix
+    ./shell.nix
+    ./terminal.nix
+    ./utils.nix
+  ];
+
+  environment.sessionVariables = { FLAKE = "nix"; };
+}
