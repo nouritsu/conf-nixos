0edfe46061e1416da4bf161a72478af32b92742d
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-17 00:35:51 +0000

    fixes #1

--- modules/home/prompt.nix
+++ modules/home/prompt.nix
@@ -8,95 +8,355 @@
     enable = true;
 
     settings = let
-      # Colors
-      prompt_ok = "purple";
-      prompt_err = "red";
-      icon = "black";
-      separator = "bright-black";
-      background = "bright-black";
+      symbols = {
+        status = {
+          success = " ";
+          failure = " ";
+          not_executable = " ";
+          not_found = " ";
+          sig_int = " ";
+          signal = "󱐋";
+        };
+        separator = "//";
+        separator_bottom = "─";
+        character = rec {
+          success = "❯";
+          error = success;
+        };
+        directory = {
+          read_only = " ";
+          truncation = "(..)/";
+        };
+        battery = {
+          full = "󰁹";
+          charging = "󰂄";
+          unknown = "󰂑";
+          empty = "󰂎";
 
-      directory = "bright-purple";
-      duration = "blue";
-      status = "red";
-      git_branch = "bright-green";
-      git_status = "bright-green";
+          "10" = "󰁺";
+          "20" = "󰁻";
+          "30" = "󰁼";
+          "40" = "󰁽";
+          "50" = "󰁾";
+          "60" = "󰁿";
+          "70" = "󰂀";
+          "80" = "󰂁";
+          "90" = "󰂂";
+        };
+
+        languages = {
+          c = " ";
+          python = " ";
+          nix = "󱄅 ";
+          go = " ";
+          rust = " ";
+          node = "󰎙 ";
+        };
+
+        package = " ";
+
+        git_branch = " ";
+
+        memory_usage = " ";
+
+        nix_shell = " ";
+      };
+
+      colors = {
+        status = {
+          default = "bold red";
+          success = "bold blue";
+          failure = "bold bright-red";
+        };
+        os = "bold blue";
+        character = {
+          success = "bold black";
+          error = "bold red";
+        };
+        username = {
+          user = "purple bold";
+          root = "red bold";
+        };
+        hostname = "bold green";
+        directory = {
+          default = "bold green";
+          read_only = "bold red";
+        };
+        separator = "bold black";
+
+        time = "bold yellow";
+
+        cmd_duration = "bold yellow";
+
+        battery = {
+          full = "bold bright-green";
+          charging = "bold bright-blue";
+          discharging = "bold red";
+          unknown = "bold yellow";
+          empty = "bold black";
+
+          "10" = "bold red";
+          "20" = "bold red";
+          "30" = "bold bright-red";
+          "40" = "bold yellow";
+          "50" = "bold yellow";
+          "60" = "bold bright-yellow";
+          "70" = "bold green";
+          "80" = "bold green";
+          "90" = "bold bright-green";
+        };
+
+        languages = {
+          c = "bold blue";
+          python = "bold yellow";
+          nix = "bold blue";
+          go = "bold cyan";
+          rust = "bold bright-red";
+          node = "bold green";
+        };
+
+        package = "bold bright-yellow";
+
+        git_branch = "bold bright-purple";
+
+        git_metrics = {
+          added = "bold green";
+          deleted = "bold red";
+        };
+
+        jobs = "bold bright-purple";
+
+        memory_usage = {
+          memory = "bold yellow";
+          percent = "bold black";
+        };
+
+        nix_shell = {
+          default = "bold blue";
+          state = "bold red";
+        };
+      };
+
+      styled = {
+        separator = "[${symbols.separator}](${colors.separator})";
+        separator_bottom = "[${symbols.separator_bottom}](${colors.separator})";
+      };
+
+      to_display = percentage: {
+        threshold = percentage;
+        style = colors.battery.${builtins.toString percentage};
+        symbol = symbols.battery.${builtins.toString percentage};
+      };
+
+      replicate = n: str: lib.concatStrings (lib.replicate n str);
+      surround = using_l: using_r: color: padding: this: "[\\${using_l}](${color})${replicate padding " "}${this}${replicate padding " "}[\\${using_r}](${color})";
+      surround_paren = this: color: padding: surround "(" ")" color padding this;
+      surround_bracket = this: color: padding: surround "[" "]" color padding this;
     in {
-      add_newline = false;
+      add_newline = true;
+      command_timeout = 2000;
+
       format = lib.concatStrings [
-        "[╭](fg:${separator})"
+        "[╭](${colors.separator})"
         "$status"
+        "$username"
+        "$hostname"
         "$directory"
         "$git_branch"
-        "$git_status"
-        "$cmd_duration"
-        "$time"
+        "$git_metrics"
+        "$fill$time$cmd_duration"
+        "$battery"
         "$line_break"
-        "[╰](fg:${separator})"
-        "[ ](blue)"
-        " ${lib.toLower (builtins.elemAt (lib.splitString " " user.name) 0)} "
+        "[╰](${colors.separator})"
+        "$memory_usage"
+        "$nix_shell"
+        "$jobs"
         "$character"
       ];
 
-      # Status
+      right_format = lib.concatStrings [
+        "$line_break"
+        "$c$python$nix$golang$rust$node"
+        "$package"
+      ];
+
+      fill = {
+        disabled = false;
+        symbol = " ";
+      };
+
       character = {
         disabled = false;
-        success_symbol = "[❯](fg:${prompt_ok})";
-        error_symbol = "[❯](fg:${prompt_err})";
+        success_symbol = "[${symbols.character.success}](${colors.character.success})";
+        error_symbol = "[${symbols.character.error}](${colors.character.error})";
+      };
+
+      line_break = {
+        disabled = false;
+      };
+
+      battery = {
+        disabled = false;
+
+        format = "${styled.separator} [$symbol$percentage%]($style) ";
+
+        full_symbol = symbols.battery.full;
+        charging_symbol = symbols.battery.charging;
+        unknown_symbol = symbols.battery.unknown;
+        empty_symbol = symbols.battery.empty;
+
+        display =
+          map
+          to_display [
+            10
+            20
+            30
+            40
+            50
+            60
+            70
+            80
+            90
+          ];
       };
 
       status = {
         disabled = false;
-        format = "[─](fg:${separator})[](fg:${status})[$symbol](fg:${icon} bg:${status})[](fg:${status} bg:${background})[ $status](bg:${background})[](fg:${background})";
+        format = "[$symbol$signal_name$maybe_int]($style) ";
+
+        style = colors.status.default;
+        success_style = colors.status.success;
+        failure_style = colors.status.failure;
+
+        symbol = symbols.status.failure;
+        success_symbol = symbols.status.success;
+        not_executable_symbol = symbols.status.not_executable;
+        not_found_symbol = symbols.status.not_found;
+        sigint_symbol = symbols.status.sig_int;
+        signal_symbol = symbols.status.signal;
 
         map_symbol = true;
-        symbol = " ";
-        not_executable_symbol = "󰜺 ";
-        not_found_symbol = " ";
-        sigint_symbol = " ";
-        signal_symbol = "󰰢 ";
-
-        pipestatus = true;
-        pipestatus_separator = "-";
-        pipestatus_segment_format = "$status";
-        pipestatus_format = "[─](fg:${separator})[](fg:${status})[$symbol](fg:${icon} bg:${status})[](fg:${status} bg:${background})[ $pipestatus](bg:${background})[](fg:${background})";
+        recognize_signal_code = true;
       };
 
-      # Directory
+      time = {
+        disabled = false;
+        style = colors.time;
+        format = "[$time]($style) ";
+        time_format = "(%A) %R";
+      };
+
+      username = {
+        disabled = false;
+        show_always = true;
+        style_user = colors.username.user;
+        style_root = colors.username.root;
+        format = "${styled.separator} [$user]($style) ";
+      };
+
+      hostname = {
+        disabled = false;
+        ssh_only = false;
+        style = colors.hostname;
+
+        format = "${styled.separator} [$hostname](bold blue) ";
+      };
 
       directory = {
         disabled = false;
-        format = "[─](fg:${separator})[](fg:${directory})[ ](fg:${icon} bg:${directory})[](fg:${directory} bg:${background})[ $path](bg:${background})[](fg:${background})";
-        truncate_to_repo = true;
-        truncation_length = 0;
+
+        format = "${styled.separator} [$path]($style)[$read_only]($read_only_style) ";
+        style = colors.directory.default;
+
+        truncation_length = 3;
+        truncation_symbol = symbols.directory.truncation;
+
+        read_only = symbols.directory.read_only;
+        read_only_style = colors.directory.read_only;
       };
 
-      directory.substitutions = {
-        "Documents" = "󰈙 ";
-        "Downloads" = " ";
-        "Music" = "󰝚 ";
-        "Pictures" = " ";
-      };
-
-      # Git
-      git_branch = {
+      c = {
         disabled = false;
-        format = "[─](fg:${separator})[](fg:${git_branch})[$symbol](fg:${icon} bg:${git_branch})[](fg:${git_branch} bg:${background})[ $branch](bg:${background})";
-        symbol = " ";
+        format = "${symbols.languages.c}\\($name-$version\\)]($style) ";
+        style = colors.languages.c;
       };
 
-      git_status = {
+      python = {
         disabled = false;
-        format = "[](fg:${background} bg:${git_status})[$all_status$ahead_behind](fg:${icon} bg:${git_status})[](fg:${git_status})";
+        format = "${symbols.languages.python}\\($version\\)]($style) ";
+        style = colors.languages.python;
       };
 
-      # Timing
+      golang = {
+        disabled = false;
+        format = "${symbols.languages.go}\\($version\\)]($style) ";
+        style = colors.languages.go;
+      };
+
+      rust = {
+        disabled = false;
+        format = "${symbols.languages.rust}\\($version\\)]($style) ";
+        style = colors.languages.rust;
+      };
+
+      nodejs = {
+        disabled = false;
+        format = "${symbols.languages.node}\\($version\\)]($style) ";
+        style = colors.languages.node;
+      };
+
+      package = {
+        disabled = false;
+        format = "${styled.separator} [${symbols.package}\\($version\\)]($style) ";
+        style = colors.package;
+      };
+
       cmd_duration = {
         disabled = false;
-        format = "[─](fg:${separator})[](fg:${duration})[󱐋](fg:${icon} bg:${duration})[](fg:${duration} bg:${background})[ $duration](bg:${background})[](fg:${background})";
         min_time = 1000;
+        format = "${styled.separator} took [$duration]($style) ";
+        show_milliseconds = true;
+        style = colors.cmd_duration;
       };
 
-      time.disabled = true;
+      git_branch = {
+        disabled = false;
+        format = "${styled.separator} [$symbol$branch(:$remote_branch)]($style) ";
+        symbol = symbols.git_branch;
+        style = colors.git_branch;
+        ignore_branches = ["master" "main"];
+      };
+
+      git_metrics = {
+        disabled = false;
+        format = "${styled.separator} ${surround_bracket "[+$added]($added_style) [-$deleted]($deleted_style)" colors.separator 1} ";
+        added_style = colors.git_metrics.added;
+        deleted_style = colors.git_metrics.deleted;
+      };
+
+      jobs = rec {
+        disabled = false;
+        format = "${styled.separator_bottom}${surround_paren "[+$number]($style)" colors.separator 0}";
+        symbol = "";
+        style = colors.jobs;
+        number_threshold = 1;
+        symbol_threshold = number_threshold;
+      };
+
+      memory_usage = {
+        disabled = false;
+        format = "${styled.separator_bottom}${surround_paren "[$symbol](${colors.memory_usage.memory})[$ram_pct]($style)" colors.separator 0}";
+        symbol = symbols.memory_usage;
+        style = colors.memory_usage.percent;
+        threshold = 50;
+      };
+
+      nix_shell = {
+        disabled = false;
+        format = "${styled.separator_bottom}${surround_bracket "[$symbol$state]($style)[$state](${colors.nix_shell.state})" colors.separator 0}";
+        symbol = symbols.nix_shell;
+        style = colors.nix_shell.default;
+      };
     };
   };
 }
