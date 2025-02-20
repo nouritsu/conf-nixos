a96d9642eb28ae4ee4c3cd37c360b44ce6477795
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-20 19:50:48 +0000

    import dconf

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,11 +1,16 @@
-{...}: {
-  imports = [
-    ./editor.nix
-    ./git.nix
-    ./prompt.nix
-    ./shell.nix
-    ./zoxide.nix
-    ./tealdeer.nix
-    ./dconf.nix
-  ];
+{
+  lib,
+  vm,
+  ...
+}: {
+  imports =
+    [
+      ./editor.nix
+      ./git.nix
+      ./prompt.nix
+      ./shell.nix
+      ./zoxide.nix
+      ./tealdeer.nix
+    ]
+    ++ lib.optionals (!vm) [./dconf.nix];
 }
