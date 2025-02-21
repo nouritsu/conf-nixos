71e5037b60836cd17ef054b9529b5599e2798721
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-21 12:25:55 +0000

    add stylix only if not in vm

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   imports =
-    [./tui.nix ./stylix.nix ./defaults.nix ./development.nix]
-    ++ lib.optionals gui [./gui.nix]
-    ++ lib.optionals (!vm) [./services.nix ./bootloader.nix ./networking.nix ./locale.nix ./dm.nix];
+    [./tui.nix ./defaults.nix ./development.nix]
+    ++ lib.optionals gui [./gui.nix ./stylix.nix ./dm.nix]
+    ++ lib.optionals (!vm) [./services.nix ./bootloader.nix ./networking.nix ./locale.nix];
 }
