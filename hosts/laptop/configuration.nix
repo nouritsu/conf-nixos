b8fc2adc9e6981a3c501a84e80be741548ffe72f
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-24 01:02:03 +0000

    fix failing builds

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,8 +1,4 @@
-{
-  user,
-  inputs,
-  ...
-}: {
+{user, ...}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -12,7 +8,7 @@
     description = user.name;
     extraGroups = ["networkmanager" "wheel"];
   };
-  inputs.nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
