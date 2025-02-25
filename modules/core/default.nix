565207287fab3990f3bea03b97e465247274ed14
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-25 19:50:42 +0000

    fix module import

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -6,6 +6,6 @@
 }: {
   imports =
     [./tui.nix ./defaults.nix ./development.nix]
-    ++ lib.optionals gui [./gui.nix ./stylix.nix ./dm.nix]
+    ++ lib.optionals gui [./gui.nix ./stylix ./dm.nix]
     ++ lib.optionals (!wsl) [./services.nix ./bootloader.nix ./networking.nix ./locale.nix];
 }
