fb299af4648d2960fc94c4e6baa5d0947a74e8f2
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-25 02:05:00 +0000

    add gestures, animations, borders

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -14,6 +14,60 @@ in {
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
 
     settings = {
+      general = {
+        gaps_in = 5;
+        gaps_out = 5;
+        border_size = 2;
+        resize_on_border = true;
+        allow_tearing = false;
+        layout = "dwindle";
+      };
+
+      decoration = rec {
+        rounding = 10;
+        active_opacity = 1.0;
+        inactive_opacity = active_opacity;
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 3;
+          new_optimizations = true;
+          vibrancy = 0.1696;
+          ignore_opacity = true;
+        };
+      };
+
+      animations = {
+        enabled = true;
+        bezier = [
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.1, 1.1, 0.1, 1.1"
+          "winOut, 0.3, -0.3, 0, 1"
+          "liner, 1, 1, 1, 1"
+        ];
+        animation = [
+          "windows, 1, 6, wind, slide"
+          "windowsIn, 1, 6, winIn, slide"
+          "windowsOut, 1, 5, winOut, slide"
+          "windowsMove, 1, 5, wind, slide"
+          "border, 1, 1, liner"
+          "borderangle, 1, 30, liner, loop"
+          "fade, 1, 10, default"
+          "workspaces, 1, 5, wind"
+        ];
+      };
+
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_fingers = 3;
+      };
+
+      dwindle = {
+        pseudotile = true;
+        preseve_split = true;
+      };
+
       bind =
         [
           "SUPER, T, exec, ${terminal}"
@@ -34,6 +88,13 @@ in {
 
           "SUPER, Q, killactive,"
         ];
+
+      misc = {
+        force_default_wallpaper = 0;
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        vrr = 0;
+      };
     };
   };
 }
