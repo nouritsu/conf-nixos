99e8ed0d3e2a7583b1667108a6f1877b48ec781b
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-25 22:03:41 +0000

    add simple waybar config

---
+++ modules/home/gui/waybar.nix
@@ -0,0 +1,48 @@
+{...}: {
+  programs.waybar = {
+    enable = true;
+    style = ./style.css;
+    systemd.enable = true;
+    settings = {
+      layer = "top";
+      position = "top";
+      mode = "dock";
+      exclusive = true;
+      passthrough = false;
+      gtk-layer-shell = true;
+      height = 0;
+
+      modules-left = [
+        "clock"
+        "hyprland/workspaces"
+      ];
+
+      modules-center = [
+        "hyprland/window"
+      ];
+
+      modules-right = [
+        "tray"
+        "memory"
+        "cpu"
+        "network"
+        "battery"
+        "backlight"
+        "pulseaudio"
+        "pulseaudio#microphone"
+      ];
+
+      "hyprland/window" = {
+        format = " {}";
+      };
+
+      "hyprland/workspaces" = {
+        disable-scroll = true;
+        all-outputs = true;
+        on-click = "activate";
+      };
+
+      tray.icon-size = 13;
+    };
+  };
+}
