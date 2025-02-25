882850686cb1ad0ba08cbfda37c39128d09bfc1b
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-25 01:31:23 +0000

    add keybinds for browser and explorer

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -2,16 +2,38 @@
   inputs,
   pkgs,
   ...
-}: {
+}: let
+  terminal = "${pkgs.foot}/bin/footclient";
+  browser = "${pkgs.google-chrome}/bin/google-chrome-stable";
+  explorer = "${pkgs.nemo-with-extensions}/bin/nemo";
+in {
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
 
     settings = {
-      bind = [
-        "SUPER, T, exec, foot"
-      ];
+      bind =
+        [
+          "SUPER, T, exec, ${terminal}"
+          "SUPER, RETURN, exec, ${terminal}"
+
+          "SUPER, W, exec, ${browser}"
+
+          "SUPER, E, exec, ${explorer}"
+        ]
+        ++ [
+          "SUPER, H, movefocus, l"
+          "SUPER, J, movefocus, d"
+          "SUPER, K, movefocus, u"
+          "SUPER, L, movefocus, r"
+          "SUPER, left, movefocus, l"
+          "SUPER, down, movefocus, d"
+          "SUPER, up, movefocus, u"
+          "SUPER, right, movefocus, r"
+
+          "SUPER, Q, killactive,"
+        ];
     };
   };
 }
