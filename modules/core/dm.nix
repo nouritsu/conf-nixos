0d170e72efe41dc49ea57fd0915189749e6fb40a
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-25 00:24:39 +0000

    change from sddm to greetd-tuigreet

--- modules/core/dm.nix
+++ modules/core/dm.nix
@@ -1,8 +1,15 @@
-{...}: {
-  services = {
-    displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
+{
+  pkgs,
+  user,
+  ...
+}: {
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
+        user = user.alias;
+      };
     };
   };
 }
