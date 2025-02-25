2fea9fe42fcd9c4dd1e8bae77f5fc9fbe9b51cbc
Aneesh Bhave <aneesh1701@gmail.com>
2025-02-25 22:16:22 +0000

    fix json type

--- modules/home/gui/waybar.nix
+++ modules/home/gui/waybar.nix
@@ -3,46 +3,48 @@
     enable = true;
     style = ./style.css;
     systemd.enable = true;
-    settings = {
-      layer = "top";
-      position = "top";
-      mode = "dock";
-      exclusive = true;
-      passthrough = false;
-      gtk-layer-shell = true;
-      height = 0;
+    settings = [
+      {
+        layer = "top";
+        position = "top";
+        mode = "dock";
+        exclusive = true;
+        passthrough = false;
+        gtk-layer-shell = true;
+        height = 0;
 
-      modules-left = [
-        "clock"
-        "hyprland/workspaces"
-      ];
+        modules-left = [
+          "clock"
+          "hyprland/workspaces"
+        ];
 
-      modules-center = [
-        "hyprland/window"
-      ];
+        modules-center = [
+          "hyprland/window"
+        ];
 
-      modules-right = [
-        "tray"
-        "memory"
-        "cpu"
-        "network"
-        "battery"
-        "backlight"
-        "pulseaudio"
-        "pulseaudio#microphone"
-      ];
+        modules-right = [
+          "tray"
+          "memory"
+          "cpu"
+          "network"
+          "battery"
+          "backlight"
+          "pulseaudio"
+          "pulseaudio#microphone"
+        ];
 
-      "hyprland/window" = {
-        format = " {}";
-      };
+        "hyprland/window" = {
+          format = " {}";
+        };
 
-      "hyprland/workspaces" = {
-        disable-scroll = true;
-        all-outputs = true;
-        on-click = "activate";
-      };
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+          on-click = "activate";
+        };
 
-      tray.icon-size = 13;
-    };
+        tray.icon-size = 13;
+      }
+    ];
   };
 }
