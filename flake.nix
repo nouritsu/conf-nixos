434591760e6f2f4d19ca63da0abde0ca6f9ea203
Aneesh Bhave <aneesh1701@gmail.com>
2025-03-26 16:27:10 +0000

    change home manager back to master, update

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
     home-manager = {
-      url = "github:nouritsu/home-manager/helix_extra_config";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
