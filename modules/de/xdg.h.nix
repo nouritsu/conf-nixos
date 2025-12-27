157839be03ffaaea4c7cffe8ad46ff07f453f574
Aneesh Bhave <aneesh1701@gmail.com>
2025-12-27 03:13:52 +0100

    remove xdg wlr package provided by hyprland home manager

--- modules/de/xdg.h.nix
+++ modules/de/xdg.h.nix
@@ -2,8 +2,8 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
     ];
+    config.common.default = ["hyprland" "gtk"];
   };
 }
