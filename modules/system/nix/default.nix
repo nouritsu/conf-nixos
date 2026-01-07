e3a9b9914991a8217596034add4044c960da1ec5
Aneesh Bhave <aneesh1701@gmail.com>
2026-01-07 20:50:06 +0100

    setup trusted users

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -7,6 +7,7 @@
   ];
 
   nixpkgs.config.allowUnfree = true;
+  nix.settings.trusted-users = ["root" "@wheel"];
   nix.settings.experimental-features = ["nix-command" "flakes"];
   programs.nix-ld.enable = true;
 }
