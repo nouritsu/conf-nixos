91a4a10e210fdc2afb7f8b8682680facc4bb1026
Aneesh Bhave <aneesh1701@gmail.com>
2026-03-29 21:54:26 +0200

    fix session command

--- modules/applications/tuigreet.nix
+++ modules/applications/tuigreet.nix
@@ -6,13 +6,22 @@
   }: let
     inherit (pkgs.stdenv.hostPlatform) system;
     tuigreet = lib.getExe pkgs.tuigreet;
-    niri-sessions = "${self.packages.${system}.niri}/share/wayland-sessions";
+    niri = self.packages.${system}.niri;
+    sessions = "/run/current-system/sw/share/wayland-sessions";
   in {
+    programs.uwsm = {
+      enable = true;
+      waylandCompositors.niri = {
+        prettyName = "Niri";
+        comment = "Niri compositor managed by UWSM";
+        binPath = "${niri}/bin/niri";
+      };
+    };
     services.greetd = {
       enable = true;
       settings = {
         default_session = {
-          command = "${tuigreet} --time --remember --remember-session --sessions ${niri-sessions}";
+          command = "${tuigreet} --time --remember --remember-session --sessions ${sessions}";
           user = "greeter";
         };
       };
--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -5,16 +5,33 @@
 
   flake.homeModules.desktop-xdg = {pkgs, ...}: let
     inherit (pkgs.stdenv.hostPlatform) system;
+    niri = self.packages.${system}.niri;
   in {
     xdg.portal = {
       enable = true;
+      configPackages = [niri];
 
       extraPortals = [
-        pkgs.xdg-desktop-portal-gnome
         pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-wlr
       ];
 
-      configPackages = [self.packages.${system}.niri];
+      config = {
+        common = {
+          default = ["gtk"];
+        };
+
+        niri = {
+          default = ["gtk"];
+          "org.freedesktop.impl.portal.ScreenCast" = ["wlr"];
+          "org.freedesktop.impl.portal.Screenshot" = ["wlr"];
+          "org.freedesktop.impl.portal.RemoteDesktop" = ["wlr"];
+          "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+          "org.freedesktop.impl.portal.AppChooser" = ["gtk"];
+          "org.freedesktop.impl.portal.OpenURI" = ["gtk"];
+          "org.freedesktop.impl.portal.Settings" = ["gtk"];
+        };
+      };
     };
   };
 }
