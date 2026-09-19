92e6497f3ec9337f8ba66863692781db5d91e6fe
Aneesh Bhave <aneesh1701@gmail.com>
2026-09-19 13:33:41 +0200

    refactor(den): derive nh and sops paths from host context

--- modules/aspects/features/secrets/secrets.nix
+++ modules/aspects/features/secrets/secrets.nix
@@ -1,17 +1,31 @@
-{inputs, ...}: {
-  den.aspects.secrets.nixos = {pkgs, ...}: {
-    imports = [inputs.sops-nix.nixosModules.sops];
+{
+  inputs,
+  lib,
+  ...
+}: {
+  den.aspects.secrets = {host, ...}: let
+    # The age identity lives in the account's home, and that same account is
+    # what reads the decrypted secrets. Hosts here carry a single one.
+    owner = lib.head (lib.attrNames host.users);
+  in {
+    nixos = {
+      pkgs,
+      config,
+      ...
+    }: {
+      imports = [inputs.sops-nix.nixosModules.sops];
 
-    environment.systemPackages = [pkgs.sops];
+      environment.systemPackages = [pkgs.sops];
 
-    sops = {
-      defaultSopsFile = ./secrets.yaml;
-      defaultSopsFormat = "yaml";
-      age.keyFile = "/home/aneesh/.config/sops/age/keys.txt";
-    };
+      sops = {
+        defaultSopsFile = ./secrets.yaml;
+        defaultSopsFormat = "yaml";
+        age.keyFile = "${config.users.users.${owner}.home}/.config/sops/age/keys.txt";
+      };
 
-    sops.secrets."home-assistant-token" = {
-      owner = "aneesh";
+      sops.secrets."home-assistant-token" = {
+        inherit owner;
+      };
     };
   };
 }
--- modules/aspects/features/system/nix.nix
+++ modules/aspects/features/system/nix.nix
@@ -1,44 +1,55 @@
-{inputs, ...}: {
-  den.aspects.nix.nixos = {
-    imports = [
-      inputs.determinate.nixosModules.default
-      inputs.nix-index-database.nixosModules.default
-    ];
+{
+  inputs,
+  lib,
+  ...
+}: {
+  den.aspects.nix = {host, ...}: let
+    # nh wants an absolute path to the flake, which only exists relative to an
+    # account. Every host here carries a single one; its home directory is set
+    # by den.batteries.define-user.
+    primary = lib.head (lib.attrNames host.users);
+  in {
+    nixos = {config, ...}: {
+      imports = [
+        inputs.determinate.nixosModules.default
+        inputs.nix-index-database.nixosModules.default
+      ];
 
-    programs.nix-ld.enable = true;
-    programs.nix-index-database.comma.enable = true;
+      programs.nix-ld.enable = true;
+      programs.nix-index-database.comma.enable = true;
 
-    programs.nh = {
-      enable = true;
-      flake = "/home/aneesh/.config/nixos";
+      programs.nh = {
+        enable = true;
+        flake = "${config.users.users.${primary}.home}/.config/nixos";
 
-      clean.enable = true;
-      clean.extraArgs = "--keep 5 --keep-since 7d";
+        clean.enable = true;
+        clean.extraArgs = "--keep 5 --keep-since 7d";
+      };
+
+      programs.fish.shellAliases.conf = "$EDITOR $NH_FLAKE";
+      programs.fish.shellAbbrs = {
+        nhrb = "nh os boot";
+        nhrs = "nh os switch";
+        nhrt = "nh os test";
+        nhca = "nh clean all";
+        nhs = "nh search";
+      };
+
+      nix.settings.trusted-users = ["root" "@wheel"];
+
+      # TODO: move to per app
+      nix.settings.extra-substituters = [
+        "https://attic.xuyh0120.win/lantian"
+        "https://cache.garnix.io"
+        "https://nix-community.cachix.org"
+        "https://yazi.cachix.org"
+      ];
+      nix.settings.trusted-public-keys = [
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+      ];
     };
-
-    programs.fish.shellAliases.conf = "$EDITOR $NH_FLAKE";
-    programs.fish.shellAbbrs = {
-      nhrb = "nh os boot";
-      nhrs = "nh os switch";
-      nhrt = "nh os test";
-      nhca = "nh clean all";
-      nhs = "nh search";
-    };
-
-    nix.settings.trusted-users = ["root" "@wheel"];
-
-    # TODO: move to per app
-    nix.settings.extra-substituters = [
-      "https://attic.xuyh0120.win/lantian"
-      "https://cache.garnix.io"
-      "https://nix-community.cachix.org"
-      "https://yazi.cachix.org"
-    ];
-    nix.settings.trusted-public-keys = [
-      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
-    ];
   };
 }
